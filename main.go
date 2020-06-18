e6b2718ad81bbd8f0c739cab3e9cf56e50a8cff6
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2020-06-18 15:05:57 +0200

    first converter, fahrenheit/kelvin to celsius

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
-1234
+package main
+
+import (
+	"fmt"
+	//"math"
+	"strings"
+)
+
+func fahrenheit(a float64) float64 {
+	b := (a-32) * 5/9
+	return b
+}
+
+func kelvin(a float64) float64 {
+	b := a - 273.15
+	return b
+}
+
+func main() {
+	fmt.Println("Fahrenheit or Kelvin to Celsius")
+	var input string
+	fmt.Scanln(&input)
+	
+	if strings.Compare(input, "Fahrenheit") == 0 {
+		fmt.Println("Type Temperature in Fahrenheit!")
+		var x float64
+		fmt.Scanln(&x)
+		fmt.Println(fahrenheit(x))
+	}
+	
+	if strings.Compare(input, "Kelvin") == 0 {
+		fmt.Println("Type Temperature in Kelvin!")
+		var x float64
+		fmt.Scanln(&x)
+		fmt.Println(kelvin(x))
+	}
+}
