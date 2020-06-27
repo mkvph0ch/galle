0dd1da7480cfa1bdffdaa04f1fdc024a0e407c3b
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2020-06-27 17:50:13 +0200

    added test.go

---
+++ test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	//"math"
+	//"strings"
+)
+
+func main {
+  var mystring string
+  mystring = "Birne"
+
+  var mystring2 string
+  mystring2 = "Apfel"
+
+  if mystring == mystring2 {
+    pmt.Println("Identical")
+  }
+
+  if mystring != mystring2 {
+    pmt.Println("Not Identical")
+  }
+}
